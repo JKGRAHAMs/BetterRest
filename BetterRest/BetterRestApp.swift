44b210b481033c59c4812acc93b796ef2911198a
Joey Graham <82058059+JKGRAHAMs@users.noreply.github.com>
2022-01-25 14:17:43 -0600

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Joey Graham on 12/28/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
